9aeae0c6e3d453cab20c0279747d6cb804d49a91
Dominik Kaiser <dkaiser@student.42heilbronn.de>
2025-02-13 12:51:25 +0100

    Add Fixed class

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,11 +6,26 @@
 /*   By: dkaiser <dkaiser@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 17:05:34 by dkaiser           #+#    #+#             */
-/*   Updated: 2025/02/12 17:05:59 by dkaiser          ###   ########.fr       */
+/*   Updated: 2025/02/13 12:50:05 by dkaiser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_H_
 #define FIXED_H_
+
+class Fixed
+{
+  public:
+    Fixed(void);
+    Fixed(const Fixed &other);
+    Fixed &operator=(const Fixed &other);
+    ~Fixed(void);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+  private:
+    static const int fractional_bits = 8;
+    int value;
+}
 
 #endif
